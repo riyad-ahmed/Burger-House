426616506e4bc1f5f9f48067cf1c67f5e98ac109
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2023-01-23 10:31:28 +0600

    change

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    parination: true
+  },
+});
+
+var swiper = new Swiper(".mySwiperTow", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
